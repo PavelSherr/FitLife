edc04243abc52715a814819c855dd2d5bfa32143
PavelSherr <sherrpasha@yandex.ru>
2026-05-05 23:41:45 +0300

    версия 5

--- fit_life.py
+++ fit_life.py
@@ -18,7 +18,7 @@ bmi = round((user_weight / user_height ** 2), 1)
 water_needed = (user_weight * 30) / 1000
 
 # 4. Вывод красивого результата
-print(f'Привет {user_name}!{user_age} г.')
+print(f'Привет {user_name}! {user_age} г.')
 print(f'индекс массы тела: {bmi}')
-print (f'необходимое колличество воды в сутки: {water_needed} л.')
+print(f'необходимое колличество воды в сутки: {water_needed} л.')
 print("Расчет окончен. Будьте здоровы!")
